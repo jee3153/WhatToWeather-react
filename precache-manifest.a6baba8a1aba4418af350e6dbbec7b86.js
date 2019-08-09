3c2787d52c8365d8ba25d0a672f0cad127d7bc48
Jenny <michel3153@gmail.com>
2019-08-09 17:25:57 +0100

    Updates

---
+++ precache-manifest.a6baba8a1aba4418af350e6dbbec7b86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0f4ff111019bf37b0e108826733ed5b",
+    "url": "/jee3153/WhatToWeather-react/index.html"
+  },
+  {
+    "revision": "0dca6fb01d4ac99ea467",
+    "url": "/jee3153/WhatToWeather-react/static/css/main.ba288421.chunk.css"
+  },
+  {
+    "revision": "1744db918d7799a6e6d6",
+    "url": "/jee3153/WhatToWeather-react/static/js/2.164617b5.chunk.js"
+  },
+  {
+    "revision": "0dca6fb01d4ac99ea467",
+    "url": "/jee3153/WhatToWeather-react/static/js/main.cde044e8.chunk.js"
+  },
+  {
+    "revision": "1f579fd6002d8a6e4876",
+    "url": "/jee3153/WhatToWeather-react/static/js/runtime~main.3b9a29ab.js"
+  }
+]);
--- src/setupTests.js
+++
@@ -1,7 +0,0 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-<<<<<<< HEAD
-=======
-
->>>>>>> test-components
-configure({ adapter: new Adapter() });
