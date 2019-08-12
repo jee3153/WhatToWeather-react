371113f5893db528f8b89a0a30097f945e9d4761
Eunjee Yang <eunjeeyang@Eunjees-iMac.local>
2019-08-12 15:40:04 +0100

    Improved clothes icons rendering

--- src/components/BottomSection/WhatToWear/WhatToWear.js
+++ src/components/BottomSection/WhatToWear/WhatToWear.js
@@ -1,55 +1,55 @@
 import React from 'react'
 
 
+
 const WhatToWear = (props) => {
 
-    function range(start, edge, step) {
-        // If only 1 number passed make it the edge and 0 the start
-        if (arguments.length === 1) {
-            edge = start;
-            start = 0;
-        }
 
-        // Validate edge/start
-        edge = edge || 0;
-        step = step || 1;
+    const curTemp = props.temperature
 
-        // Create array of numbers, stopping before the edge
-        let arr = [];
-        for (arr; (edge - start) * step > 0; start += step) {
-            arr.push(start);
-        }
-        return arr;
+    const top = {
+        'coat': { 'temp': curTemp >= 10 && curTemp < 14 },
+        'jacket': { 'temp': curTemp >= 14 && curTemp < 18 },
+        'hoodie': { 'temp': curTemp >= 18 && curTemp < 21 },
+        'paddedjacket': { 'temp': curTemp < 10 },
+        'tanktop': { 'temp': curTemp > 29 },
+        'longshirt': { 'temp': curTemp >= 21 && curTemp < 25 },
+        'tshirt': { 'temp': curTemp >= 25 && curTemp < 31 },
     }
 
+    const bottom = {
+        'longtrousers': { 'temp': curTemp < 26 },
+        'shorttrousers': { 'temp': curTemp > 25 }
+    }
 
-    const clothes = [
-        ['boots', ...range(-40, 10)],
-        ['coat', ...range(10, 14)],
-        ['flipflop', ...range(29, 51)],
-        ['hat', ...range(-40, 10)],
-        ['hoodie', ...range(18, 21)],
-        ['jacket', ...range(14, 18)],
-        ['longshirt', ...range(21, 25)],
-        ['longtrousers', ...range(-40, 26)],
-        ['paddedjacket', ...range(-40, 10)],
-        ['shoes', ...range(10, 23)],
-        ['shorttrousers', ...range(26, 51)],
-        ['tanktop', ...range(30, 51)],
-        ['trainers', ...range(23, 29)],
-        ['tshirt', ...range(25, 31)]]
+    const shoe = {
+        'boots': { 'temp': curTemp < 10 },
+        'flipflop': { 'temp': curTemp > 28 },
+        'trainers': { 'temp': curTemp >= 23 && curTemp < 29 },
+        'shoes': { 'temp': curTemp >= 10 && curTemp < 23 }
+    }
 
-    const decisionMaker = (temp) => {
-        let itemArr = []
-        for (let i = 0; i < clothes.length; i++) {
-            if (clothes[i].includes(temp)) {
-                itemArr.push(clothes[i][0])
+    const hat = {
+        'hat': { 'temp': curTemp < 10 }
+    }
+
+    const decisionMaker = (type) => {
+        for (let key in type) {
+            if (type[key]['temp']) {
+                return (
+                    <img className='WhatToWear__outfit__icons__icon' src={`${process.env.PUBLIC_URL}/images/clothes/${key}.svg`} alt="outfit for today" />
+                )
             }
+
         }
-        return itemArr
+
+
+
+
+
     }
-    // console.log(props.currently)
-    // console.log(decisionMaker(props.temperature));
+
+
 
     const outfitSuggestion = (temp) => {
         let msg;
@@ -86,11 +86,13 @@ const WhatToWear = (props) => {
                 </div>
             )}
             <div className='WhatToWear__outfit'>
-                {decisionMaker(props.temperature).map((item, index) => (
-                    <div className='WhatToWear__outfit__icons' key={index}>
-                        <img className='WhatToWear__outfit__icons__icon' src={`${process.env.PUBLIC_URL}/images/clothes/${item}.svg`} alt="outfit for today" />
-                    </div>
-                ))}
+                <div className='WhatToWear__outfit__icons'>
+                    {decisionMaker(top)}
+                    {decisionMaker(bottom)}
+                    {decisionMaker(shoe)}
+                    {decisionMaker(hat)}
+                </div>
+
                 {outfitSuggestion(props.temperature)}
             </div>
 
@@ -100,3 +102,50 @@ const WhatToWear = (props) => {
 }
 
 export default WhatToWear
+
+// function range(start, edge, step) {
+    //     // If only 1 number passed make it the edge and 0 the start
+    //     if (arguments.length === 1) {
+    //         edge = start;
+    //         start = 0;
+    //     }
+
+    //     // Validate edge/start
+    //     edge = edge || 0;
+    //     step = step || 1;
+
+    //     // Create array of numbers, stopping before the edge
+    //     let arr = [];
+    //     for (arr; (edge - start) * step > 0; start += step) {
+    //         arr.push(start);
+    //     }
+    //     return arr;
+    // }
+
+// const clothes = [
+    //     ['boots', ...range(-40, 10)],
+    //     ['coat', ...range(10, 14)],
+    //     ['flipflop', ...range(29, 51)],
+    //     ['hat', ...range(-40, 10)],
+    //     ['hoodie', ...range(18, 21)],
+    //     ['jacket', ...range(14, 18)],
+    //     ['longshirt', ...range(21, 25)],
+    //     ['longtrousers', ...range(-40, 26)],
+    //     ['paddedjacket', ...range(-40, 10)],
+    //     ['shoes', ...range(10, 23)],
+    //     ['shorttrousers', ...range(26, 51)],
+    //     ['tanktop', ...range(30, 51)],
+    //     ['trainers', ...range(23, 29)],
+    //     ['tshirt', ...range(25, 31)]]
+
+    // const decisionMaker = (temp) => {
+    //     let itemArr = []
+    //     for (let i = 0; i < clothes.length; i++) {
+    //         if (clothes[i].includes(temp)) {
+    //             itemArr.push(clothes[i][0])
+    //         }
+    //     }
+    //     return itemArr
+    // }
+    // console.log(props.currently)
+    // console.log(decisionMaker(props.temperature));
